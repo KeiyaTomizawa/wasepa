a341236c915c5d591200f97450c1eb56f13b37d1
keiya <k.tommy75.japan@gmail.com>
2016-05-25 18:54:18 +0900

    remake hello/models.py

--- mysite/hello/models.py
+++ mysite/hello/models.py
@@ -1,23 +1,20 @@
 from __future__ import unicode_literals
-
 from django.db import models
+
 
 # Create your models here.
-from django.core.db import models
-
-
-class Customer(models.Model):
-      # id = AutoField(primary_key=True)  # 自動的に追加されるので定義不要
-      last_name = CharField(max_length=20)
-      first_name = CharField(max_length=20)
-      email_address = EmailField(max_length=255, unique=True)
-      memo = TextField(null=True)
-
-from django.db import models
-
-
-class Hello(models.Model):
-      your_name = models.CharField(max_length=10)
-
-      def __str__(self):
-        return "<{0}>".format(self.your_name)
+#from django.core.db import models
+#
+#
+#class Customer(models.Model):
+#      # id = AutoField(primary_key=True)  # 自動的に追加されるので定義不要
+#      last_name = CharField(max_length=20)
+#      first_name = CharField(max_length=20)
+#      email_address = EmailField(max_length=255, unique=True)
+#      memo = TextField(null=True)
+#
+#class Hello(models.Model):
+#      your_name = models.CharField(max_length=10)
+#
+#      def __str__(self):
+#        return "<{0}>".format(self.your_name)
